ee12e1c99cb316241c5ce48c10bf6e31f90f6a88
Dariusz Sikora <dev@isangeles.pl>
2024-06-20 22:13:21 +0200

    deleting build directory after creating the release artifact

--- release.ps1
+++ release.ps1
@@ -11,11 +11,14 @@ make build
 # Create build artifact.
 # Build name contains version from VERSION file, date string, and os + arch info.
 $buildName = "openelwynn_$(type VERSION)_$(Get-Date -Format "yyyyMMdd")_windows_x86_64.zip"
-Compress-Archive -Path ./* -CompressionLevel Fastest -DestinationPath ../$($buildName)
+Compress-Archive -Path ./* -CompressionLevel Fastest -DestinationPath ./$($buildName)
+# Cleanup.
+cd ..
+rm -rf openelwynn
 # Upload to the release server.
 $creds = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes("$($login):$($pass)"))
 $headers = @{
     Authorization = "Basic $creds";
 }
-$body = "file=$(get-content ../$buildName -raw)"
-Invoke-WebRequest -Uri "https://my.opendesktop.org/remote.php/dav/files/isangeles/openelwynn/release/$buildName" -Method PUT -InFile ../$buildName -ContentType "application/octet-stream" -Headers $headers
+$body = "file=$(get-content ./$buildName -raw)"
+Invoke-WebRequest -Uri "https://my.opendesktop.org/remote.php/dav/files/isangeles/openelwynn/release/$buildName" -Method PUT -InFile ./$buildName -ContentType "application/octet-stream" -Headers $headers
